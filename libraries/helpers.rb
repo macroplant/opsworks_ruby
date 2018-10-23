2dfeaf1536a3bc79bf1b92a58d92cf6d27dee513
chrisdevor <chrisdevor@macroplant.com>
2018-10-23 19:56:52 -0400

    Update libraries/helpers.rb

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -79,7 +79,7 @@ def deploy_dir(application)
 end
 
 def every_enabled_application
-  node['deploy'].keys.each do |deploy_app_shortname|
+  node['deploy'].each_key do |deploy_app_shortname|
     application = applications.detect { |app| app['shortname'] == deploy_app_shortname }
     next unless application && application['deploy']
 
